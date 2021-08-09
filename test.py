7f6f34c28928aeee7f6315fea8e954a9bc399346
JustinD-Dev <39993170+justind-dev@users.noreply.github.com>
2021-08-09 16:20:43 -0400

    Raising exceptions
    Raising exceptions in functions

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello World")
+def calc_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less")
+    return 10/age
+
+try:
+    calc_xfactor(-1.5)
+except ValueError as error:
+    print(error)
