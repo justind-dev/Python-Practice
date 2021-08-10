0c9fcafdb3915b1f0263ae974f0df5438651e81b
JustinD-Dev <39993170+justind-dev@users.noreply.github.com>
2021-08-09 20:26:36 -0400

    Create ClassInheritance.py
    In this file, we explore class creation, inheritance in sub classes, including inherited methods and attributes.

---
+++ ClassInheritance.py
@@ -0,0 +1,36 @@
+class Animal:
+    def __init__(self) -> None:
+        self.age = 1
+        pass
+    def eat(self):
+        print("eat")
+
+# Animal: Parent, Base
+# Mammal: Child, Sub
+class Mammal(Animal):
+    def walk(self):
+        print("walk")
+
+# Animal: Parent, Base
+# Fish: Child, Sub
+class Fish(Animal):
+
+    def swim(self):
+        print("swim")
+
+# Define our objects
+man = Mammal()
+salmon = Fish()
+
+# Check that they inherited parent class methods
+man.eat()
+salmon.eat()
+
+# Check that they inherited parent class attributes
+print(man.age)
+
+# modify attribs
+salmon.age = 99
+
+# and finally check
+print(salmon.age)
